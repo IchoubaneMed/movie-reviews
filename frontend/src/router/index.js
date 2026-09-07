c588119fd5be1bef4bc267c2484c95fc6cbb0618
mohamed_ichoubane <mohamed.ichoubane@nestle.com>
2026-09-07 18:26:29 +0100

    feat: configure application routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "movies",
+      component: () =>
+        import("../views/MovieListView.vue"),
+    },
+    {
+      path: "/movies/:id",
+      name: "movie-detail",
+      component: () =>
+        import("../views/MovieDetailView.vue"),
+      props: true,
+    },
+  ],
 })
 
 export default router
---
+++ frontend/src/views/MovieDetailView.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div>Movie {{ id }}</div>
+</template>
