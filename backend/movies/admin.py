acce9e98f9a9cb4f51066146f1275ec55d91e567
mohamed_ichoubane <mohamed.ichoubane@nestle.com>
2026-09-07 16:54:20 +0100

    feat: configure Django admin

--- backend/movies/admin.py
+++ backend/movies/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import Actor, Movie, Review
+
 # Register your models here.
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)
+    filter_horizontal = ("actors",)
+    inlines = [ReviewInline]
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name")
+    search_fields = ("first_name", "last_name")
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "movie", "grade")
+    list_filter = ("grade",)
